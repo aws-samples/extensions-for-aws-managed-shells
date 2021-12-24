d3a45282891001ac35d263cabf113f9059d8b996
Massimo Re Ferre <massimo@it20.info>
2021-12-24 15:49:25 +0100

    Fixed a number of different redirects (-L) in curls + pip2 install

--- extensions-for-aws-managed-shells.sh
+++ extensions-for-aws-managed-shells.sh
@@ -27,6 +27,7 @@ export PATH=/usr/local/bin/code-server/bin:$PATH
 # setup various utils (latest at time of docker build)
 # docker is being installed to support DinD scenarios (e.g. for being able to build)
 # httpd-tools include the ab tool (for benchmarking http end points)
+yum install sudo -y 
 sudo yum update -y \
  && sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
  && sudo yum install -y \
@@ -80,7 +81,7 @@ mkdir -p ${NVM_DIR} \
 npm install -g typescript
 
 # setup pip (latest at time of docker build)
-curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py \
+curl -s https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py \
    && python get-pip.py
  
 ########################################
@@ -114,8 +115,8 @@ curl -Ls "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2
  && sudo ./aws/install --update \
  && /usr/local/bin/aws --version
 
- # setup the eb cli (latest at time of docker build)
-python -m pip install awsebcli --upgrade 
+# setup the eb cli (latest at time of docker build)
+pip3 install awsebcli --upgrade  
 
 # setup the aws cdk cli (latest at time of docker build)
 sudo npm i -g aws-cdk
@@ -140,7 +141,7 @@ curl -sLO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s h
     && sudo mv ./kubectl /usr/local/bin/kubectl
 
 # setup kubecolor 
-LATEST=$(curl -s https://api.github.com/repos/dty1er/kubecolor/releases/latest) \
+LATEST=$(curl -sL https://api.github.com/repos/dty1er/kubecolor/releases/latest) \
 && X86URL=$(echo $LATEST | jq -r '.assets[].browser_download_url' | grep Linux_x86_64.tar.gz) \
 && X86ARTIFACT=$(echo $LATEST  | jq -r '.assets[].name' | grep Linux_x86_64.tar.gz) \
 && curl -L -O $X86URL \
@@ -171,7 +172,7 @@ curl "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/1/artifacts/eks-a
 sudo mv ./eksctl-anywhere /usr/local/bin/
 
 # setup kubecfg 
-LATEST=$(curl -s https://api.github.com/repos/bitnami/kubecfg/releases/latest) \
+LATEST=$(curl -sL https://api.github.com/repos/bitnami/kubecfg/releases/latest) \
 && X86URL=$(echo $LATEST | jq -r '.assets[].browser_download_url' | grep linux-amd64) \
 && curl -sLo kubecfg $X86URL \
 && chmod +x ./kubecfg \
@@ -247,7 +248,7 @@ fi
 if [ "$AWS_EXECUTION_ENV" != "CloudShell" ]
     then
         if [ -d "/usr/local/bin/code-server-dir" ]; then sudo rm -Rf /usr/local/bin/code-server-dir; sudo rm /usr/local/bin/code-server; fi
-        LATEST=$(curl -s https://api.github.com/repos/cdr/code-server/releases/latest) \
+        LATEST=$(curl -sL https://api.github.com/repos/cdr/code-server/releases/latest) \
         && X86URL=$(echo $LATEST | jq -r '.assets[].browser_download_url' | grep linux-amd64) \
         && X86ARTIFACT=$(echo $LATEST  | jq -r '.assets[].name' | grep linux-amd64) \
         && curl -L -O $X86URL \
